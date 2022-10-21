0ec212e6311a05351ef6c42f2093554b85b7aa22
Danny_Ivanov <nokia5230dok@yandex.ru>
2022-10-21 14:44:47 +0700

    well done

--- lab6.js
+++ lab6.js
@@ -1,12 +1,16 @@
 function myLeft() {
+    event.preventDefault();
+
     document.getElementById('left_column').style.width = "75%";
     document.getElementById('right_column').style.width = "25%";
 }
 function myCenter() {
+    event.preventDefault();
     document.getElementById('left_column').style.width = "50%";
     document.getElementById('right_column').style.width = "50%";
 }
 function myRight() {
+    event.preventDefault();
     document.getElementById('left_column').style.width = "25%";
     document.getElementById('right_column').style.width = "75%";
 }
